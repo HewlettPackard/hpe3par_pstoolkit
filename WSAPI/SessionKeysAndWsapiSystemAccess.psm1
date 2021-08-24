dd71befdc873c8771220e74fe6b5b76254d5743d
zweailltienrger <70688129+zweailltienrger@users.noreply.github.com>
2021-08-24 15:49:09 +0200

    Update SessionKeysAndWsapiSystemAccess.psm1
    When using this in IIS PowerShell isn't able to compile C# code on the fly. In my opinion it'd be better to let the user handle self-signed certificate issues. Possible solution would be to write an assembly for TrustAllCertsPolicy :
    Add-Type -TypeDefinition @"
        using System.Net;
        using System.Security.Cryptography.X509Certificates;
        public class TrustAllCertsPolicy : ICertificatePolicy {
            public bool CheckValidationResult(
                ServicePoint srvPoint, X509Certificate certificate,
                WebRequest request, int certificateProblem) {
                return true;
            }
        }
    "@ -OutputAssembly C:\Temp\TrustAllCertsPolicy.dll
    
    and later on load it:
    Add-Type -Path C:\Temp\TrustAllCertsPolicy.dll
    [System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy

--- WSAPI/SessionKeysAndWsapiSystemAccess.psm1
+++ WSAPI/SessionKeysAndWsapiSystemAccess.psm1
@@ -100,30 +100,6 @@ Function New-WSAPIConnection {
 			[System.String]
 			$ArrayType
 		)
-#(self-signed) certificate,
-
-if ($PSEdition -eq 'Core')
-{
-    
-} 
-else 
-{
-
-add-type @" 
-using System.Net; 
-using System.Security.Cryptography.X509Certificates; 
-public class TrustAllCertsPolicy : ICertificatePolicy { 
-	public bool CheckValidationResult( 
-		ServicePoint srvPoint, X509Certificate certificate, 
-		WebRequest request, int certificateProblem) { 
-		return true; 
-	} 
-} 
-"@  
-		[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-}
-
-#END of (self-signed) certificate,
 		if(!($SANPassword))
 		{
 			$SANPassword1 = Read-host "SANPassword" -assecurestring
